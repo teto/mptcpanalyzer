6e839784306f1b23372a44b3c64819ecc0afef37
Matthieu Coudron <mcoudron@hotmail.com>
2021-01-01 21:25:12 +0100

    adding flake.nix

--- flake.nix
+++ flake.nix
@@ -3,15 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # temporary until this gets fixed upstream
+    poetry.url = "github:teto/poetry2nix/fix_tag";
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: let
-    mptcpanalyzer = nixpkgs.lib.callPackage ./default.nix {};
-  in {
+  outputs = { self, nixpkgs, flake-utils, poetry }: let
+  in flake-utils.lib.eachDefaultSystem (system: let
+    # myPoetry = nixpkgs.legacyPackages.${system}.poetry2nix;
+    myPoetry = poetry.packages.poetry;
+    in rec {
 
-    packages.x86_64-linux.mptcpanalyzer = mptcpanalyzer;
+    packages.mptcpanalyzer = myPoetry.mkPoetryApplication {
+      projectDir = ./.;
+      preferWheels = false;
+      overrides = myPoetry.overrides.withDefaults (self: super: {
 
-    devShell = mptcpanalyzer;
-    defaultPackage = mptcpanalyzer;
-  };
+        matplotlib = (super.matplotlib.override {enableGtk3=true;}).overrideAttrs(oa: {
+          buildInputs = oa.buildInputs ++ [ nixpkgs.legacyPackages.${system}.pango ];
+          strictDeps = false;
+        });
+      });
+    };
+    devShell = packages.mptcpanalyzer;
+    defaultPackage = packages.mptcpanalyzer;
+  });
 }
