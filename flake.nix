fd5ce77f0f71eb56b9a7c5ad8d343da532309dc5
Matthieu Coudron <mcoudron@hotmail.com>
2021-09-21 15:44:48 +0200

    added haskell-language-server cache

--- flake.nix
+++ flake.nix
@@ -11,6 +11,14 @@
   #   # ];
   #   # bash-prompt = "toto";
   # };
+  nixConfig = {
+    substituters = [
+        "https://haskell-language-server.cachix.org"
+    ];
+    trusted-public-keys = [
+      "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
+    ];
+  };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
